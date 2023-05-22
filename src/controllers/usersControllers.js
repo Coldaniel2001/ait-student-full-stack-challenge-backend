02ccfc1ef515a451fbdda1a789538af19aad765f
Daniel Wilson <danielvillenawilson@gmail.com>
2023-05-22 20:33:06 +0200

    change(add functions agregate)

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -12,16 +12,19 @@ const getAllUsers = async (req, res) => {
 const createUser = async (req, res) => {
     const { name, email } = req.body
     try {
-
-        const user = await UserModel.findOne({ email: email })
-        if (user) {
+        const user = await UserModel.aggregate(
+            [
+                {
+                    $match: { email: email }
+                }
+            ])
+        if (user.length !== 0) {
             return res.status(200).send({ status: 'False', msg: "This User Exist" })
         }
         const newUser = await UserModel.create({
             name,
             email
         })
-        console.log(newUser)
         return res.status(200).send({ status: 'OK', newUser })
 
     } catch (error) {
@@ -32,7 +35,12 @@ const createUser = async (req, res) => {
 const getUser = async (req, res) => {
     const { userId } = req.params
     try {
-        const user = await UserModel.findOne({ email: userId })
+        const user = await UserModel.aggregate(
+            [
+                {
+                    $match: { email: userId }
+                }
+            ])
         res.status(200).send({ status: 'OK', user })
     } catch (error) {
         res.status(500).send({ status: 'FALSE' })
